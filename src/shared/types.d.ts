646e3e557036ba58498f841eb5d305ca7adc484d
samuel.gomez <contact@samuel.gomez.fr>
2022-08-22 14:55:20 +0200

    docs(types): shared types

--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -3,4 +3,20 @@ export type Tanomaly = {
   detail?: string;
   type?: string;
   iconName?: string;
+  code?: string | number;
+};
+
+export type TInitialState = {
+  isLoading: boolean;
+  isLoaded: boolean;
+  [x: string]: unknown;
+  anomaly: {
+    [x: string]: Tanomaly | null;
+  };
+};
+
+export type TFetchState = TInitialState & {
+  label?: string;
+  code?: string | number;
+  detail?: string;
 };
