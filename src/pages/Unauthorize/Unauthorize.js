d83ea3fa1f1c93fad05cce1e1ce3ecb0dcac3957
samuel.gomez <contact@samuel.gomez.fr>
2022-08-24 23:35:02 +0200

    feat(pageunauthorize): migration typescript

--- src/pages/Unauthorize/Unauthorize.js
+++
@@ -1,9 +0,0 @@
-import ResiliencePage from 'shared/components/ResiliencePage';
-import { TITLE, MESSAGE, CODE } from './constants';
-import './Unauthorize.scss';
-
-const Unauthorize = () => (
-  <ResiliencePage title={TITLE} message={MESSAGE} code={CODE} subtitlePartOne="forbidden" backhome={false} classModifier="unauthorize" />
-);
-
-export default Unauthorize;
--- src/pages/Unauthorize/__tests__/Unauthorize.spec.tsx
+++ src/pages/Unauthorize/__tests__/Unauthorize.spec.tsx
@@ -1,28 +1,24 @@
-import { render } from '@testing-library/react';
-import { StaticRouter } from 'react-router-dom/server';
+import { screen } from '@testing-library/react';
+import { renderWithWrapperStaticRouter, emptyFunction } from 'shared/testsUtils';
 import { defineFeature, loadFeature } from 'jest-cucumber';
-import { emptyFunction } from 'shared/testsUtils';
 import Unauthorize from '../Unauthorize';
-
-const wrapper = ({ children }) => <StaticRouter context={{}}>{children}</StaticRouter>;
 
 const feature = loadFeature('src/pages/Unauthorize/Unauthorize.feature');
 
 defineFeature(feature, test => {
   test('Chargement de la page 403', ({ given, when, then, and }) => {
-    let $;
     given('Je suis un utilisateur accédant à une page non autorisée', emptyFunction);
 
     when('Je suis redirigé sur cette page', () => {
-      $ = render(<Unauthorize />, { wrapper });
+      renderWithWrapperStaticRouter(<Unauthorize />);
     });
 
     then(/^La page m'affiche un titre "403notauthorized"$/, () => {
-      expect($.getByText('403 Forbidden')).toBeDefined();
+      expect(screen.getByText('403 Forbidden')).toBeDefined();
     });
 
     and(/^La page m'affiche le contenu suivant "The page you are looking for is forbidden !"$/, () => {
-      expect($.getByText('The page you are looking for is forbidden !')).toBeDefined();
+      expect(screen.getByText('The page you are looking for is forbidden !')).toBeDefined();
     });
   });
 });
