4bc604620a0b2101dd669e7fed4264a87736e2ae
samuel.gomez <contact@samuel.gomez.fr>
2022-08-24 23:36:25 +0200

    feat(commonmodal): add hook

---
+++ src/shared/components/ModalCommon/__tests__/ModalCommon.hook.test.ts
@@ -0,0 +1,18 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useToggleModal from '../ModalCommon.hook';
+
+describe('useToggleModal', () => {
+  it('Should isOpenModal to be false when closeModal called', () => {
+    const { result } = renderHook(() => useToggleModal());
+    act(() => result.current.onCancel());
+    const resIsOpenModal = result.current.isOpen;
+    expect(resIsOpenModal).toBe(false);
+  });
+
+  it('Should isOpenModal to be true when openModal called', () => {
+    const { result } = renderHook(() => useToggleModal());
+    act(() => result.current.openModal());
+    const resIsOpenModal = result.current.isOpen;
+    expect(resIsOpenModal).toBe(true);
+  });
+});
