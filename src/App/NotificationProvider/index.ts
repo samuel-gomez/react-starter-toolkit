ab3d89b267a63bf630304f450285ed82c23f5756
samuel.gomez <contact@samuel.gomez.fr>
2022-08-24 23:30:16 +0200

    docs(notification): expose return type

--- src/App/NotificationProvider/index.ts
+++ src/App/NotificationProvider/index.ts
@@ -1,4 +1,5 @@
 export { default, NotificationContext } from './NotificationProvider';
 export { default as useNotifications } from './Notifications.hook';
+export type { TReturnUseNotification } from './Notifications.hook';
 export type { TaddNotification, TonDeleteNotification } from './Notifications.hook';
 export type { EType } from './Notifications';
