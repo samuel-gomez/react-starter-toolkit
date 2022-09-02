7406305b17450e6fcae5ec61cbdc73458b620b25
samuel.gomez <contact@samuel.gomez.fr>
2022-09-02 23:51:37 +0200

    refactor(spec): path to feature

--- src/shared/testsUtils/mocks/handlers.js
+++ src/shared/testsUtils/mocks/handlers.js
@@ -1,34 +1,6 @@
 import { rest } from 'msw';
 import { MOCK_API_URL } from '..';
 
-/* export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    sessionStorage.setItem('is-authenticated', 'true');
-
-    return res(ctx.status(200));
-  }),
-
-  rest.get('/user', (req, res, ctx) => {
-    const isAuthenticated = sessionStorage.getItem('is-authenticated');
-
-    if (!isAuthenticated) {
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
-      );
-    }
-
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
-    );
-  }),
-]; */
-
 const commonResponse = (req, res, ctx) => {
   const testMock = req.headers.get('testMock');
   const testMockParsed = JSON.parse(testMock);
